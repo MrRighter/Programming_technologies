3084ec1334a08c9c135a4ac2283ad6f86aba57ae
MrRighter <123870295+MrRighter@users.noreply.github.com>
2026-02-14 22:49:18 +0300

    Добавьте файлы проекта.

---
+++ Practical work 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practical_work_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
