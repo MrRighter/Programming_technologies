8a3f3f7a7d7df6f06268c44793999931fe40f941
MrRighter <123870295+MrRighter@users.noreply.github.com>
2026-04-12 23:57:16 +0300

    first practical

---
+++ Practical work 1/1_Task2.cs
@@ -0,0 +1,28 @@
+﻿////Написать функцию `bool CheckDuplicates(int[] nums)`, где:
+////Функция должна вернуть `true`, если в массиве `nums` есть хотя бы один повторяющийся элемент. В противном случае, функция должна вернуть `false`.
+////Нельзя использовать сортировку.
+
+
+
+//namespace Practical_work_1 {
+//    internal class _1_Task2 {
+//        static void Main() {
+//            Console.Write("Напишите массив чисел через пробел: ");
+//            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+//            Console.WriteLine(CheckDuplicates(nums));
+//        }
+
+//        static bool CheckDuplicates(int[] nums) {
+//            for (int i = 0; i < nums.Length; i++) {
+//                for (int j = i + 1; j < nums.Length; j++) {
+//                    if (nums[i] == nums[j]) {
+//                        return true;
+//                    }
+//                }
+//            }
+
+//            return false;
+//        }
+//    }
+//}
---
+++ Practical work 1/1_Task4.cs
@@ -0,0 +1,27 @@
+﻿////Дана строка `s`. Напишите функцию, которая проверяет, является ли строка `s` палиндромом.
+////Строка `s` является палиндромом, если она читается одинаково как справа налево, так и слева направо. Перед проверкой на палиндром нужно удалить из строки все не алфавитно-цифровые символы.
+////Нельзя использовать метод `Reverse`.
+
+
+
+//namespace Practical_work_1 {
+//    internal class _1_Task4 {
+//        static void Main() {
+//            string palindrome = "А роза упала на лапу Азора!";
+//            string clean_palindrome = string.Concat(palindrome.Where(char.IsLetter)).ToLower();
+
+//            string new_cleanP = "";
+
+//            for (int i = clean_palindrome.Length - 1; i > -1; i--) {
+//                new_cleanP += clean_palindrome[i];
+//            }
+
+//            if (clean_palindrome == new_cleanP) {
+//                Console.WriteLine("Палиндром!");
+//            }
+//            else {
+//                Console.WriteLine("Не палиндром!");
+//            }
+//        }
+//    }
+//}
