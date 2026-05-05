513db6eea295a06623e97225953f1c28b8ca6949
MrRighter <123870295+MrRighter@users.noreply.github.com>
2026-05-05 10:39:43 +0300

    upd task5 and task6

--- Practical work 2/2_Task5.cs
+++ Practical work 2/2_Task5.cs
@@ -1,16 +1,82 @@
-﻿//Необходимо создать свой класс представляющий стек, который поддерживает операции добавления, удаления, получения верхнего элемента и получения минимального элемента за константное время.
-//Необходимо реализовать класс `MinStack` со следующими методами:
-//- `MinStack()` -конструктор класса;
-//- `void Push(int val)` -добавляет элемент `val` на вершину стека;
-//- `void Pop()` -удаляет элемент, находящийся на вершине стека;
-//- `int Top()` -возвращает верхний элемент стека;
-//- `int GetMin()` -возвращает минимальный элемент стека.
-//Все методы должны работать за O(1) время. 
-//Внутренне стэк должен использовать односвязаный список.
+﻿////Необходимо создать свой класс представляющий стек, который поддерживает операции добавления, удаления, получения верхнего элемента и получения минимального элемента за константное время.
+////Необходимо реализовать класс `MinStack` со следующими методами:
+////- `MinStack()` -конструктор класса;
+////- `void Push(int val)` -добавляет элемент `val` на вершину стека;
+////- `void Pop()` -удаляет элемент, находящийся на вершине стека;
+////- `int Top()` -возвращает верхний элемент стека;
+////- `int GetMin()` -возвращает минимальный элемент стека.
+////Все методы должны работать за O(1) время. 
+////Внутренне стек должен использовать односвязный список.
 
 
 
-namespace Practical_work_2 {
-    internal class _2_Task5 {
-    }
-}
+//namespace Practical_work_2 {
+//    internal class _2_Task5 {
+//        static void Main() {
+//            MinStack stack = new MinStack();
+//            stack.Push(4);
+//            stack.Push(8);
+//            stack.Push(7);
+//            stack.Push(1);
+//            stack.Push(9);
+
+//            Console.WriteLine(stack.Top());
+//            Console.WriteLine(stack.GetMin());
+
+//            stack.Pop();
+
+//            Console.WriteLine(stack.Top());
+//            Console.WriteLine(stack.GetMin());
+
+//            stack.Push(-5);
+//            stack.Push(5);
+
+//            Console.WriteLine(stack.Top());
+//            Console.WriteLine(stack.GetMin());
+//        }
+//    }
+
+//    public class MinStack {
+//        private class Node {
+//            public int Value;
+//            public int Min;
+//            public Node Next;
+
+//            public Node(int value, int min, Node next = null) {
+//                Value = value;
+//                Min = min;
+//                Next = next;
+//            }
+//        }
+
+//        private Node top;
+
+//        public MinStack() {
+//            top = null;
+//        }
+
+//        public void Push(int val) {
+//            if (top == null)
+//                top = new Node(val, val);
+//            else
+//                top = new Node(val, Math.Min(val, top.Min), top);
+//        }
+
+//        public void Pop() {
+//            if (top != null)
+//                top = top.Next;
+//        }
+
+//        public int Top() {
+//            if (top == null)
+//                return 0;
+//            return top.Value;
+//        }
+
+//        public int GetMin() {
+//            if (top == null)
+//                return 0;
+//            return top.Min;
+//        }
+//    }
+//}
--- Practical work 2/2_Task6.cs
+++ Practical work 2/2_Task6.cs
@@ -1,9 +1,36 @@
-﻿//Дано `n` пар скобок. Напишите функцию, которая генерирует все возможные комбинации правильных скобочных последовательностей.
-//Необходимо написать функцию `List<str> GenerateParentheses(int n)`, которая будет принимать на вход целое число `n` - количество пар скобок, и возвращать список строк - все возможные правильные скобочные последовательности.
+﻿////Дано `n` пар скобок. Напишите функцию, которая генерирует все возможные комбинации правильных скобочных последовательностей.
+////Необходимо написать функцию `List<str> GenerateParentheses(int n)`, которая будет принимать на вход целое число `n` - количество пар скобок, и возвращать список строк - все возможные правильные скобочные последовательности.
 
 
 
-namespace Practical_work_2 {
-    internal class _2_Task6 {
-    }
-}
+//namespace Practical_work_2 {
+//    internal class _2_Task6 {
+//        static void Main() {
+//            Console.Write("Введите n: ");
+//            int n = int.Parse(Console.ReadLine());
+
+//            List<string> result = GenerateParentheses(n);
+//            Console.WriteLine(string.Join(", ", result));
+//        }
+
+//        static List<string> GenerateParentheses(int n) {
+//            List<string> result = new List<string>();
+
+//            void Backtrack(string current, int open, int close) {
+//                if (current.Length == 2 * n) {
+//                    result.Add(current);
+//                    return;
+//                }
+
+//                if (open < n)
+//                    Backtrack(current + '(', open + 1, close);
+
+//                if (close < open)
+//                    Backtrack(current + ')', open, close + 1);
+//            }
+
+//            Backtrack("", 0, 0);
+//            return result;
+//        }
+//    }
+//}
